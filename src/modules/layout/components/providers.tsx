2a80cd4b636dbc017bd4162a8409c2402302abca
abdullah <abdullah981066@gmail.com>
2025-11-20 19:08:50 +0530

    chore(setup): bootstrap project

---
+++ src/modules/layout/components/providers.tsx
@@ -0,0 +1,18 @@
+import { ThemeProvider } from "@/modules/layout/components/theme-provider";
+import { TRPCReactProvider } from "@/trpc/client";
+import { ReactNode } from "react";
+
+export function Providers({ children }: { children: ReactNode }) {
+  return (
+    <TRPCReactProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </TRPCReactProvider>
+  );
+}
